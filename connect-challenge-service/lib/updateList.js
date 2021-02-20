159ce775eebd804a9a921bf5b9081ff8f2c6277e
Taylor Widman <tpwidman@gmail.com>
2021-02-20 17:14:43 -0600

    Adds update list script for filtering word dictionary locally

---
+++ connect-challenge-service/lib/updateList.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const check = require('wordlist-english');
+const englishWords = check["english"];
+const { words } = require("./word-dictionary.json");
+const filtered = words.filter(word => word.length <= 7 && englishWords.indexOf(word) > -1);
+fs.writeFileSync("./words.json", JSON.stringify(filtered, null, 2));
