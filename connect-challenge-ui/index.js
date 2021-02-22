28fbba60ea71e45d192df08becfc9d44bb0996d2
Taylor Widman <tpwidman@gmail.com>
2021-02-22 16:06:57 -0600

    Adds lambda function js

--- connect-challenge-ui/index.js
+++ connect-challenge-ui/index.js
@@ -2,12 +2,27 @@ const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB();
 exports.handler = async (event) => {
     const params = {
+      Limit: 5,
       TableName: process.env.TABLE_NAME
     }
-    const results = await docClient.scan(params).promise();
+    const result = await docClient.scan(params).promise();
+    const output = result.Items.map(item => {
+        const phoneNumber = item.phoneNumber.S;
+        const bestVanityOrdered = item.bestVanityOrdered.L;
+        const createdAt = item.createdAt.S;
+        return {
+            "phoneNumber": phoneNumber,
+            "bestVanityOrdered": bestVanityOrdered.map(({ S }) => S).join(', '),
+            "createdAt": createdAt
+        }
+    })
     const response = {
         statusCode: 200,
-        body: JSON.stringify(results.Items),
+          headers: {
+            "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+            "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS 
+          },
+        body: JSON.stringify(output)
     };
     return response;
 };
